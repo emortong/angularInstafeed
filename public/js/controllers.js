58b4ccaeab098e128cf106b596e41bd786b929eb
Estefania Morton <estefania.morton@gmail.com>
2017-01-31 14:38:48 -1000

    mvp don, search feature

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,6 +9,15 @@
         $scope.instafeed = [];
         InstaFeed.getInstaFeed()
           .then( res => {
+            let feed = res.data.data.map( feed => {
+              console.log(feed);
+              if(feed.type === 'video') {
+                feed.url = feed.videos.low_resolution.url;
+                return feed;
+              } else {
+                feed.url = feed.images.standard_resolution.url
+              }
+            })
             $scope.instafeed = res.data.data;
             console.log($scope.instafeed);
           })
